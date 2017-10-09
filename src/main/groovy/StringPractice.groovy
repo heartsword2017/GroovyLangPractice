8971e27b60322299910414f86e2bbf79a84bbb38
heartsword <xyb5b@mail.missouri.edu>
2017-10-08 20:21:47 -0500

    string practice

--- src/main/groovy/StringPractice.groovy
+++
@@ -1,6 +0,0 @@
-/**
- * Created by xinjianyao on 10/6/17.
- */
-class StringPractice {
-
-}
---
+++ src/main/groovy/com/xinjian/practice/StringPractice.groovy
@@ -0,0 +1,42 @@
+package com.xinjian.practice
+
+import org.codehaus.groovy.syntax.Numbers
+
+/**
+ * Created by xinjianyao on 10/6/17.
+ */
+class StringPractice {
+
+    static void main(String[] args) {
+        String a = 'single quoted string is a java string'
+        String b = '''triple quoted string 
+                    can
+                    be
+                    multiple
+                    lines'''
+        String c = "double quoted string is a java string without interpolation"
+        int d = 1312
+        String e = "Number is $d" + ". Double quoted string is a GString if interpolation is present"
+//        String f = "Number is $d.toString()" + "will fail cuz dollar sign is for dotted expression, use \${} instead"
+        String f = "Number is ${d.toString()}"
+        String g ="""Triple double quoted strings behave like double quoted strings, 
+                    with the addition that they are multiline
+                    and the value of number is $d"""
+
+        String h = /slashy string is good for regx, no need to escape backslash/
+        String i = /slashy string can
+                    also be multiple lines/
+        String j = /slashy string also
+                    support interpolation so
+                    the value of the number is ${d}/
+
+
+        println e
+        println f
+        println g
+        println i
+        println j
+    }
+
+
+}
